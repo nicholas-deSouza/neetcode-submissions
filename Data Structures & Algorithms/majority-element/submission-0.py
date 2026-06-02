1096fc95ee543e53600732b69ed23f2b880553da
Nicholas de Souza <64615525+nicholas-deSouza@users.noreply.github.com>
2026-06-01 20:35:27 -0700

    Add: majority-element - submission-0

---
+++ Data Structures & Algorithms/majority-element/submission-0.py
@@ -0,0 +1,13 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        
+        half = len(nums)/2
+        
+        hashMap = {}
+
+        for i, num in enumerate(nums):
+            hashMap[num] = hashMap.get(num,0) + 1
+        
+        for i, num in enumerate(hashMap):
+            if hashMap[num] > half:
+                return num
